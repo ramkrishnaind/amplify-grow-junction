98132018f2c859c32989d563c86338481c45c91f
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-11-18 13:15:46 +0530

    Working code with auth and API

---
+++ pages/add-todo.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { createTodo } from "../src/graphql/mutations";
+import { v4 as uuid } from "uuid";
+const AddTodo = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const nameChangeHandler = (e) => {
+    setName(e.target.value);
+  };
+  const descriptionChangeHandler = (e) => {
+    setDescription(e.target.value);
+  };
+  const submitHandler = async (e) => {
+    const id = uuid();
+    e.preventDefault();
+    await API.graphql({
+      query: createTodo,
+      variables: { input: { name, description, id } },
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    });
+    console.log("success");
+  };
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label>
+            Name:
+            <input type="text" value={name} onChange={nameChangeHandler} />
+          </label>
+          <label>
+            Description:
+            <input
+              type="text"
+              value={description}
+              onChange={descriptionChangeHandler}
+            />
+          </label>
+        </div>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default withAuthenticator(AddTodo);
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo(
+    $filter: ModelSubscriptionTodoFilterInput
+    $username: String
+  ) {
+    onCreateTodo(filter: $filter, username: $username) {
+      id
+      name
+      username
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo(
+    $filter: ModelSubscriptionTodoFilterInput
+    $username: String
+  ) {
+    onUpdateTodo(filter: $filter, username: $username) {
+      id
+      name
+      username
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo(
+    $filter: ModelSubscriptionTodoFilterInput
+    $username: String
+  ) {
+    onDeleteTodo(filter: $filter, username: $username) {
+      id
+      name
+      username
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
