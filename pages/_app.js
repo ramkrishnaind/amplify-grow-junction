486e258aef5e27c354f3b18b13932f78be6360c3
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-12-24 16:35:18 +0530

    Bug resoved for useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 // import '../amplify-configure'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from '../redux/store'
@@ -16,38 +16,11 @@ import { Formik } from 'formik'
 // add to existing imports
 import Amplify from 'aws-amplify'
 import config from '../src/aws-exports'
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from '@auth0/auth0-react'
 // check if env is localhost or not
 // if you're not developing on localhost, you will need to detect this is another way—the docs linked above give some examples.
 function MyApp({ Component, pageProps }) {
-  const [host,setHost]= useState()
-  debugger
-  // console.log('toast', toast)
-  const router = useRouter()
-  if (router.asPath.includes('/mentor/')) {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Mentor>
-            <Component {...pageProps} />
-          </Mentor>
-        </PersistGate>
-        <ToastContainer />
-      </Provider>
-    )
-  }
-  if (router.asPath.includes('/student/')) {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Student>
-            <Component {...pageProps} />
-          </Student>
-        </PersistGate>
-        <ToastContainer />
-      </Provider>
-    )
-  }
+  const [host, setHost] = useState()
   useEffect(() => {
     const isLocalhost = !!(window.location.hostname === 'localhost')
 
@@ -75,6 +48,49 @@ function MyApp({ Component, pageProps }) {
     setHost(window.location.origin)
     Amplify.configure(updatedAwsConfig)
   }, [])
+  // console.log('toast', toast)
+  const router = useRouter()
+  if (router.asPath.includes('/mentor/')) {
+    return (
+      host && (
+        <Auth0Provider
+          domain="dev-tra4scc70r3ty8yh.us.auth0.com"
+          clientId="QQR0KU0g8usdgAxqAyFEpWrx5EKreM7P"
+          redirectUri={host}
+        >
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <Mentor>
+                <Component {...pageProps} />
+              </Mentor>
+            </PersistGate>
+            <ToastContainer />
+          </Provider>
+        </Auth0Provider>
+      )
+    )
+  }
+  if (router.asPath.includes('/student/')) {
+    return (
+      host && (
+        <Auth0Provider
+          domain="dev-tra4scc70r3ty8yh.us.auth0.com"
+          clientId="QQR0KU0g8usdgAxqAyFEpWrx5EKreM7P"
+          redirectUri={host}
+        >
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <Student>
+                <Component {...pageProps} />
+              </Student>
+            </PersistGate>
+            <ToastContainer />
+          </Provider>
+        </Auth0Provider>
+      )
+    )
+  }
+
   // if (router.asPath.includes('/services/')) {
   //   return (
   //     <Provider store={store}>
@@ -88,19 +104,20 @@ function MyApp({ Component, pageProps }) {
   //   )
   // }
   return (
-    host && <Auth0Provider
-      domain="dev-tra4scc70r3ty8yh.us.auth0.com"
-      clientId="QQR0KU0g8usdgAxqAyFEpWrx5EKreM7P"
-      redirectUri={host}
-    >
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
-        <ToastContainer />
-      </Provider>
+    host && (
+      <Auth0Provider
+        domain="dev-tra4scc70r3ty8yh.us.auth0.com"
+        clientId="QQR0KU0g8usdgAxqAyFEpWrx5EKreM7P"
+        redirectUri={host}
+      >
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
+          <ToastContainer />
+        </Provider>
       </Auth0Provider>
-    
+    )
   )
 }
 
