fff54dd9de48245c95ba381894b2877083177c28
saianita <50478345+saianita@users.noreply.github.com>
2022-12-29 15:22:51 +0530

    Profile top nav image changes

--- components/Mentor/LeftMenu/Expanded/Author/index.js
+++ components/Mentor/LeftMenu/Expanded/Author/index.js
@@ -8,14 +8,10 @@ const Author = () => {
   const [image, setImage] = useState('')
   const [name, setName] = useState()
   const [url, setUrl] = useState()
-  const [userInfo, setUserInfo] = useState(false)
-  const [convertedImage, setConvertedImage] = useState()
-  const [usrName, setUsrName] = useState()
 
-  const getUser = async () => {
-    debugger
-    const usrName = getLoggedinUserEmail()
-    setUsrName(usrName)
+  const getUrl = async () => {
+      const usrName = getLoggedinUserEmail()
+    console.log("username - ", usrName)
     const results = await API.graphql(
       graphqlOperation(listMentorRegisters, {
         filter: { username: { contains: usrName } },
@@ -23,25 +19,16 @@ const Author = () => {
     )
     if (results.data.listMentorRegisters.items.length > 0) {
       const data = { ...results.data.listMentorRegisters.items[0] }
+      debugger
       if (data.profile_image) {
         const img = await Storage.get(data.profile_image)
         console.log("image - ", img)
         setImage(img)
-        //setImage(await Storage.get(data.profile_image))
       }
       debugger
-      // const name = data.about_yourself.first_name + ' ' + data.about_yourself.last_name
-      // const url = data.about_yourself.grow_junction_url
-      // setName(name)
-      // setUrl(url)
-      setName(data.about_yourself.first_name + ' ' + data.about_yourself.last_name)
-      setUrl(data.about_yourself.grow_junction_url)
-      setUserInfo(true)
-      console.log("name-",name)
-      console.log("url -", url)
-      console.log("image - ", image)
-
-      if (!userInfo) {
+      setName(data.about_yourself?.first_name + ' ' + data.about_yourself?.last_name)
+      setUrl(data.about_yourself?.grow_junction_url)
+      if (!data.profile_image) {
         const results = await API.graphql(
           graphqlOperation(listUserInfos, {
             filter: { username: { contains: usrName } },
@@ -55,7 +42,7 @@ const Author = () => {
             setImage(img)
           }
           if (!name) {
-            const name = data.name
+            const name = data?.name
             //const url = data.grow_junction_url
             setName(name)
           }
@@ -65,21 +52,27 @@ const Author = () => {
   }
 
   useEffect(() => {
-    getUser()
+    getUrl()
   }, [])
+
+
   return (
     <section
       className={`${classes.container} h-60 bg-yellow-100 p-4 flex flex-col mb-15`}
     >
       <section className="mb-8 flex">
         <div className={`${classes['persona']} bg-gray-300 rounded-full`}>
-          {image !== 'undefined' ? (
+          {image ? (
             <img
-              src={URL.createObjectURL(image)}
+              src={image}
               alt=""
               className={`${classes['persona']} rounded-full`}
             />
-          ) : null}
+          ) : <img
+          src=""
+          alt=""
+          className={`${classes['persona']} rounded-full`}
+        />}
         </div>
         <article className="flex flex-col mt-3">
           <p className='text-base font-semibold'>{name}</p>
--- components/Mentor/MainContent/Header/index.js
+++ components/Mentor/MainContent/Header/index.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { Auth } from 'aws-amplify'
+import { Auth, API, Storage, graphqlOperation } from 'aws-amplify'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useDispatch } from 'react-redux'
 import { ClearUser, StoreUserAuth } from '../../../../redux/actions/AuthAction'
 import useComponentVisible from '../../../../hooks/useComponentVisible'
 import ReactDOM from 'react-dom'
+import {
+  listUserInfos,
+  listMentorRegisters,
+} from '../../../../src/graphql/queries'
+import { getLoggedinUserEmail } from '../../../../utilities/user'
+
 const Header = () => {
   const [open, setOpen] = useState(false)
+  const [image, setImage] = useState('')
+
   const { ref, isComponentVisible, setIsComponentVisible } =
     useComponentVisible(false)
   const dispatch = useDispatch()
@@ -16,6 +24,11 @@ const Header = () => {
     debugger
     setOpen(isComponentVisible)
   }, [isComponentVisible])
+
+  useEffect(() => {
+    getUrl()
+  }, [])
+
   console.log('isComponentVisible', isComponentVisible)
   console.log('open', open)
   const { logout: oAuthLogout } = useAuth0()
@@ -30,6 +43,41 @@ const Header = () => {
     } catch (error) {}
   }
   const mentorHeader = useSelector((state) => state.MentorHeaderReducer)
+
+  const getUrl = async () => {
+    const usrName = getLoggedinUserEmail()
+    console.log('username - ', usrName)
+    const results = await API.graphql(
+      graphqlOperation(listMentorRegisters, {
+        filter: { username: { contains: usrName } },
+      }),
+    )
+    if (results.data.listMentorRegisters.items.length > 0) {
+      const data = { ...results.data.listMentorRegisters.items[0] }
+      debugger
+      if (data.profile_image) {
+        const img = await Storage.get(data.profile_image)
+        console.log('image - ', img)
+        setImage(img)
+      }
+      if (!data.profile_image) {
+        const results = await API.graphql(
+          graphqlOperation(listUserInfos, {
+            filter: { username: { contains: usrName } },
+          }),
+        )
+        if (results.data.listUserInfos.items.length > 0) {
+          debugger
+          const data = { ...results.data.listUserInfos.items[0] }
+          if (data.profile_image) {
+            const img = await Storage.get(data.profile_image)
+            setImage(img)
+          }
+        }
+      }
+    }
+  }
+
   return (
     <div
       ref={ref}
@@ -63,10 +111,7 @@ const Header = () => {
               setIsComponentVisible(true)
             }}
           >
-            <img
-              className=" w-full object-cover"
-              src={`/assets/icon/mentor-dashboard/persona.png`}
-            />
+            <img className=" w-full object-cover" src={image} />
           </li>
         </ul>
       </nav>
